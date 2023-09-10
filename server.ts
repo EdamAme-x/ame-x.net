09c921056f36b24894f060cbfad2b8cfb3b55f7d
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-09-10 20:55:21 +0900

    a

--- server.ts
+++ server.ts
@@ -31,6 +31,7 @@ export function app(): express.Express {
   // All regular routes use the Universal engine
   server.get('*', (req, res) => {
     res.setHeader('x-powered-by', 'Next.js 99999999;version:9999999');
+    res.setHeader('x-development-by', '@amex2189');
     res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
   });
 
