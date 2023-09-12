8e89dbcc183b7a919aed5c416eb4d7ef0494a12d
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-09-12 17:28:15 +0900

    f menu and png

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -7,11 +7,26 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   isOpen = false;
+  menuOpacity = 100;
   changeMenu() {
     this.isOpen = !this.isOpen;
   }
 
   toPage(url: string) {
     window.location.href = url;
+  }
+
+  ngAfterViewInit() {
+    if (typeof window === 'undefined') {
+      return;
+    }
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        this.menuOpacity = 0;
+      } else {
+        this.menuOpacity = 100
+      }
+    })
   }
 }
