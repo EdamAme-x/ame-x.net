8c454d950937a10c3008dff8be1b1513b39d8bac
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-09-12 16:18:33 +0900

    convert png to webp

---
+++ api/convert.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
+
+const directoryPath = path.join(__dirname, '../src/assets');
+
+fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    files.forEach(function (file) {
+        if (path.extname(file) === '.png') {
+            let fileName = path.basename(file, '.png');
+            sharp(directoryPath + '/' + file)
+                .webp({ progressive: true })
+                .toFile(directoryPath + '/' + fileName + '.webp', (err, info) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(info);
+                    }
+                });
+        }
+    });
+});
