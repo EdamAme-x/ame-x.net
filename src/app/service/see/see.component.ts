441af33a7780799e6ec2f2fa41a3d87ca91be56d
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-08 16:01:07 +0900

    f

--- src/app/service/see/see.component.ts
+++ src/app/service/see/see.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ServiceSee',
   templateUrl: './see.component.html',
-  styleUrls: ['./see.component.scss']
+  styleUrls: ['./see.component.scss'],
 })
 export class SeeComponent {
-
+  toPage(url: string) {
+    window.location.href = url;
+  }
 }
