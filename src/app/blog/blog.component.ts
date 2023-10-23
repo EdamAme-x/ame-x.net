73513de923f88aad0a9bd0b2bbd93b655b774e0c
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-23 19:14:04 +0900

    add

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -4,12 +4,10 @@ import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
 })
 export class BlogComponent implements OnInit {
-  constructor(
-    private titleService: Title
-  ) { }
+  constructor(private titleService: Title) {}
 
   ngOnInit(): void {
     this.titleService.setTitle('Ame_x BLOG');
@@ -19,45 +17,66 @@ export class BlogComponent implements OnInit {
     }
 
     if (localStorage.getItem('articles') == null) {
-      fetch('/model/articles').then(
-        (res) => res.json()
-      ).then((d) => {
-        this.articles = d.data;
-        // cache
-        localStorage.setItem('articles', JSON.stringify(this.articles));
-        localStorage.setItem('expiry_2', Date.now().toString());
-      })
-    } else {
-
-      if (Date.now() - parseInt(localStorage.getItem('expiry_2') || Date.now().toString()) > 1000 * 60 * 60 * 24) {
-        fetch('/model/articles').then(
-          (res) => res.json()
-        ).then((d) => {
+      fetch('/model/articles')
+        .then((res) => res.json())
+        .then((d) => {
           this.articles = d.data;
-          // cahce
+          // cache
           localStorage.setItem('articles', JSON.stringify(this.articles));
           localStorage.setItem('expiry_2', Date.now().toString());
-        })
+        });
+    } else {
+      if (
+        Date.now() -
+          parseInt(localStorage.getItem('expiry_2') || Date.now().toString()) >
+        1000 * 60 * 60 * 24
+      ) {
+        fetch('/model/articles')
+          .then((res) => res.json())
+          .then((d) => {
+            this.articles = d.data;
+            // cahce
+            localStorage.setItem('articles', JSON.stringify(this.articles));
+            localStorage.setItem('expiry_2', Date.now().toString());
+          });
       }
 
       this.articles = JSON.parse(localStorage.getItem('articles') as string);
     }
+
+    this.orderDateArticle = Object.create(this.articles);
+    this.orderLikeArticle = Object.create(this.articles).sort(
+      (a: any, b: any) => {
+        return b.liked_count - a.liked_count;
+      }
+    );
   }
 
-  articles: { [key: string]: any }[] = [];
+  articles: { [key: string]: any }[] = []; // View
+  orderDateArticle: { [key: string]: any }[] = []; // order date
+  orderLikeArticle: { [key: string]: any }[] = []; // order like article
 
   convertToUserTime(input: string): string {
     try {
-        const parsedDate = new Date(input);
-        const year = parsedDate.getFullYear();
-        const month = parsedDate.getMonth() + 1;
-        const day = parsedDate.getDate();
-        const hours = parsedDate.getHours();
-        const minutes = parsedDate.getMinutes();
-        return `${year} ${month}/${day} ${hours}:${minutes}`;
+      const parsedDate = new Date(input);
+      const year = parsedDate.getFullYear();
+      const month = parsedDate.getMonth() + 1;
+      const day = parsedDate.getDate();
+      const hours = parsedDate.getHours();
+      const minutes = parsedDate.getMinutes();
+      return `${year} ${month}/${day} ${hours}:${minutes}`;
     } catch (error) {
-        return "invaid date";
+      return 'invaid date';
     }
-}
+  }
 
+  sortBy(e: Event | any) {
+    if (e.target?.value == '更新順') {
+      this.articles = Object.create(this.orderDateArticle);
+    } else if (e.target?.value == '評価順') {
+      this.articles = Object.create(this.orderLikeArticle);
+    }
+
+    console.log(e.target?.value);
+  }
 }
