13461567c9c970f6ff84eeba2156e4818f217e34
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-25 00:46:36 +0900

    add menu

--- src/app/layout/header/menu/menu.component.ts
+++ src/app/layout/header/menu/menu.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+type Nav = {
+  name: string;
+  path: `/${string}`;
+  icon: `https://${string}`;
+  class?: string;
+};
+
 @Component({
   selector: 'LayoutHeaderMenu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent {
-
+  navs: Nav[] = [
+    {
+      name: 'Career',
+      path: '/career',
+      icon: 'https://www.svgrepo.com/show/500614/management.svg',
+      class: 'filter invert',
+    },
+  ];
 }
